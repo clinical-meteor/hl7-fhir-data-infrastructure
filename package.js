09b280e1295842fc56f8d49ac5c900ef2934b89f
Abigail Watson <awatson1978@gmail.com>
2020-03-11 21:06:33 -0500

    npm dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-fhir-data-infrastructure',
-  version: '6.1.22',
+  version: '6.1.23',
   summary: 'HL7 FHIR Data Infrastructure (SimpleSchemas, Cursors, Hooks)',
   git: 'https://github.com/clinical-meteor/hl7-fhir-data-infrastructure',
   documentation: 'README.md'
@@ -119,13 +119,13 @@ Package.onUse(function (api) {
 
 
 Npm.depends({
-  // "moment": "2.22.2",
-  // "lodash": "4.17.13",
-  // "material-fhir-ui": "0.9.32",
-  // "react-icons-kit": "1.3.1",
-  // "prop-types": "15.7.2",
-  // "react-mixin": "4.0.0",
-  // "simpl-schema": "1.5.3",
-  // "validator": "10.9.0",
+  "moment": "2.22.2",
+  "lodash": "4.17.13",
+  "material-fhir-ui": "0.9.32",
+  "react-icons-kit": "1.3.1",
+  "prop-types": "15.7.2",
+  "react-mixin": "4.0.0",
+  "simpl-schema": "1.5.3",
+  "validator": "10.9.0"
 });
 
