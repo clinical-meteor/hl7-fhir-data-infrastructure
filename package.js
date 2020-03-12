a9b82373ef6fe5b7c58588946b0c5d4d11f7b15d
Abigail Watson <awatson1978@gmail.com>
2020-03-11 22:04:11 -0500

    no npm deps; moved to main app

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-fhir-data-infrastructure',
-  version: '6.1.27',
+  version: '6.1.28',
   summary: 'HL7 FHIR Data Infrastructure (SimpleSchemas, Cursors, Hooks)',
   git: 'https://github.com/clinical-meteor/hl7-fhir-data-infrastructure',
   documentation: 'README.md'
@@ -114,14 +114,14 @@ Package.onUse(function (api) {
 });
 
 
-Npm.depends({
-  "moment": "2.22.2",
-  "lodash": "4.17.13",
-  "prop-types": "15.7.2",
-  "react-mixin": "4.0.0",
-  "validator": "10.9.0",
-  "simpl-schema": "1.5.3",
-  "material-fhir-ui": "0.9.34"
-  // "react-icons-kit": "1.3.1",
-});
+// Npm.depends({
+//   // "moment": "2.22.2"
+//   // "lodash": "4.17.13",
+//   // "prop-types": "15.7.2",
+//   // "react-mixin": "4.0.0",
+//   // "validator": "10.9.0",
+//   // "simpl-schema": "1.5.3",
+//   // "material-fhir-ui": "0.9.34"
+//   // "react-icons-kit": "1.3.1",
+// });
 
