c7ac78b406f6494b4d599c6ce7c22e7bc03ada3c
Abigail Watson <awatson1978@gmail.com>
2020-03-11 20:07:29 -0500

    meteor-base; ecmascript

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-fhir-data-infrastructure',
-  version: '6.1.12',
+  version: '6.1.13',
   summary: 'HL7 FHIR Data Infrastructure (SimpleSchemas, Cursors, Hooks)',
   git: 'https://github.com/clinical-meteor/hl7-fhir-data-infrastructure',
   documentation: 'README.md'
@@ -10,8 +10,8 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  // api.use('meteor-base@1.4.0');
-  // api.use('ecmascript@0.13.0');
+  api.use('meteor-base@1.4.0');
+  api.use('ecmascript@0.13.0');
   // api.use('session');
   // api.use('mongo');
   // api.use('http');
