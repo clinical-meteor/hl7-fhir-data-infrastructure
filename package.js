30aa75b34f1660a5e5423edea84ead085848df1e
Abigail Watson <awatson@mitre.org>
2021-12-01 21:34:34 -0600

    react npm dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-fhir-data-infrastructure',
-  version: '6.19.0',
+  version: '6.19.1',
   summary: 'HL7 FHIR Data Infrastructure (SimpleSchemas, Cursors, Hooks)',
   git: 'https://github.com/clinical-meteor/hl7-fhir-data-infrastructure',
   documentation: 'README.md'
@@ -422,5 +422,6 @@ Package.onUse(function (api) {
 
 Npm.depends({
   "react-sortable-hoc": "1.11.0",
-  "react-icons-kit": "2.0.0"
+  "react-icons-kit": "2.0.0",
+  "react": "16.13.0"
 })
