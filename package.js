3f5cd7a2daeb86b457f50717ac2184bbfd05e10c
Abigail Watson <awatson@mitre.org>
2022-10-31 22:32:00 -0500

    geomapping

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-fhir-data-infrastructure',
-  version: '6.27.5',
+  version: '6.27.7',
   summary: 'HL7 FHIR Data Infrastructure (SimpleSchemas, Cursors, Hooks)',
   git: 'https://github.com/clinical-meteor/hl7-fhir-data-infrastructure',
   documentation: 'README.md'
